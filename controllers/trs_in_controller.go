0c1266b4184ad1b35370a2bd87a87328f8a84a98
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-21 22:47:02 +0700

    update for controller here deployer

--- controllers/trs_in_controller.go
+++ controllers/trs_in_controller.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
-
 	"samb-backend/config"
 	"samb-backend/models"
 
@@ -21,6 +21,45 @@ func CreateTrxIn(c *gin.Context) {
 
 func GetTrxIn(c *gin.Context) {
 	var headers []models.TrxInHeader
-	config.DataBase.Preload("Details").Find(&headers)
+	rows,err := config.DataBase.Raw(`SELECT * FROM TransaksiPenerimaanBarangHeader`,).Rows()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("Error fetching products data: %s", err.Error()),
+		})
+		return 
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var header models.TrxInHeader
+		if err := rows.Scan(&header.TrxInPK, &header.TrxInNo, &header.WhsIdf, &header.TrxInDate, &header.TrxInSuppIdf, &header.TrxInNotes); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Fetch details for each header
+		var details []models.TrxInDetail
+		detailRows, err := config.DataBase.Raw(
+			`SELECT * FROM TransaksiPenerimaanBarangDetail WHERE TrxInIDF = ?`,
+			header.TrxInPK,
+		).Rows()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer detailRows.Close()
+
+		for detailRows.Next() {
+			var detail models.TrxInDetail
+			if err := detailRows.Scan(&detail.TrxInDPK, &detail.TrxInIDF, &detail.TrxInDProductIdf, &detail.TrxInDQtyDus, &detail.TrxInDQtyPcs); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			details = append(details, detail)
+		}
+
+		header.Details = details
+		headers = append(headers, header)
+	}
+
 	c.JSON(http.StatusOK, headers)
 }
