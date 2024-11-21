1598c7ea2dd7f434ace80c669be9af11979b1243
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-21 20:45:43 +0700

    update for customer routes

--- routes/routes.go
+++ routes/routes.go
@@ -8,20 +8,17 @@ import (
 
 func RegisteredRoutes( )*gin.Engine {
 	router := gin.Default()
-	// For master data
 	router.GET("/suppliers", controllers.GetSuppliers)
 	router.GET("/customers", controllers.GetCustomers)
 	router.GET("/products", controllers.GetProducts)
 	router.GET("/warehouses", controllers.GetWarehouses)
 
-	// For transactions
 	router.POST("/trx-in", controllers.CreateTrxIn)
 	router.GET("/trx-in", controllers.GetTrxIn)
 
 	router.POST("/trx-out", controllers.CreateTrxOut)
 	router.GET("/trx-out", controllers.GetTrxOut)
 
-	// For stock report
 	router.GET("/stock", controllers.GetStockReport)
 	return router
 }
