663f1d556f6535c7cad60eacc5f0dcf22d6eaa70
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-21 23:53:29 +0700

    update for database here

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 var DataBase*gorm.DB
 
 func InitDB() *gorm.DB {
-	network := "admin:password@tcp(api-database-1.cluster-crasa40cmvqz.ap-southeast-2.rds.amazonaws.com:3306)/api-database-1?charset=utf8mb4&parseTime=True&loc=Local"
+	network := "admin:password@tcp(api-database-1.cluster-ro-crasa40cmvqz.ap-southeast-2.rds.amazonaws.com:3306)/api-database-1?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err := gorm.Open(mysql.Open(network), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Connection failed to database: %v", err)
